e3c67b367d20966266f038a983c902d8b67e3874
pandoraxcc <60555311+pandoraxcc@users.noreply.github.com>
2021-05-07 23:49:12 -0500

    Added incapsulation

--- simple_OOP.py
+++ simple_OOP.py
@@ -61,3 +61,58 @@ a = Employee('Rahul', 886012, 200000, "Intern")
   
 # calling a function of the class Person using its instance
 a.display() 
+
+
+#Example of incapsulation
+
+# Python program to
+# demonstrate private members
+ 
+# Creating a Base class
+class Base:
+    def __init__(self):
+        self.a = "Pandorax"
+        self.__c = "Cybersecurity"
+ 
+# Creating a derived class
+class Derived(Base):
+    def __init__(self):
+         
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        print("Calling private member of base class: ")
+        print(self.__c)
+# Driver code
+obj1 = Base()
+print(obj1.a) # Prints public 
+print(obj1.c) # Causing attribute error, because protected and can't be accessed from derived class
+
+
+
+# Python program to
+# demonstrate protected members
+ 
+# Creating a base class
+class Base:
+    def __init__(self):
+         
+        # Protected member
+        self._a = 2
+ 
+# Creating a derived class   
+class Derived(Base):
+    def __init__(self):
+         
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        #print("Calling protected member of base class: ")
+        #print(self._a)
+ 
+obj1 = Derived()
+
+obj2 = Base()
+ 
+print(obj2.a) # will cause error
+ 
